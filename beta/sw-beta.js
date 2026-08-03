4fea7ab954c42a90a68f56c49cfcd70bb471dbbc
raphaalbrecht <raphael@exaustech.com.br>
2026-08-03 16:32:49 -0300

    Beta: cadastro de assinante pede CPF antes do nome (Direct Data primeiro)

--- beta/sw-beta.js
+++ beta/sw-beta.js
@@ -2,7 +2,7 @@
    (mata o cache de 10 min do GitHub Pages); offline cai na última cópia guardada. Usa cache próprio
    (exaustech-beta-*) e NÃO deleta o cache do app da raiz. Bump BETA_SW_VERSION pra forçar atualização
    do próprio SW. Adicionado 27/07/2026 por pedido do Raphael (revertendo o "beta sem SW"). */
-const BETA_SW_VERSION = '2026-07-27-1';
+const BETA_SW_VERSION = '2026-08-03-1';
 const CACHE = 'exaustech-beta-' + BETA_SW_VERSION;
 
 self.addEventListener('install', function(e){ self.skipWaiting(); });
